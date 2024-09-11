828c68070cbaa526ff01ae10da12b5685e3cdfdf
YeongHunYun <compyyh@gmail.com>
2024-09-11 18:18:56 +0900

    깃 최초 등록

---
+++ src/main/java/org/suhodo/sb_ex01/SbEx01Application.java
@@ -0,0 +1,13 @@
+package org.suhodo.sb_ex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEx01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbEx01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/suhodo/sb_ex01/SbEx01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.suhodo.sb_ex01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEx01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
